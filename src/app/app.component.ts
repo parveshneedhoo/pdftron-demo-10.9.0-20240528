59a99922375c58982672a861d6eac890850da774
parveshneedhoo <parveshneedhoo@spoonconsulting.com>
2024-05-29 10:24:16 +0400

    pdftron 10.9.0-20240528

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ import { Core } from '@pdftron/webviewer';
 export class AppComponent implements AfterViewInit {
   @ViewChild('viewer') viewer!: ElementRef;
 
-  fieldValues = {};
   docViewer: any;
   annotManager: any;
   fieldManager: any;
@@ -19,7 +18,6 @@ export class AppComponent implements AfterViewInit {
   instance: any;
   fieldChangesDebounced: any;
   onError: any;
-  photoFieldPositions = {};
   
   @Output() coreControlsEvent:EventEmitter<string> = new EventEmitter();
 
@@ -27,6 +25,7 @@ export class AppComponent implements AfterViewInit {
 
     const options = {
       path: "../lib",
+      licenseKey: "demo:1702924333888:7c8fe3900300000000b1eb3aad22888de45c07dd78b662456b14de4103",
       disabledElements: [
         "header",
         "toolsHeader",
@@ -57,90 +56,11 @@ export class AppComponent implements AfterViewInit {
     WebViewer(options, this.viewer.nativeElement).then(async (instance) => {
       this.instance = instance;
       instance.UI.iframeWindow.addEventListener("loaderror", this.onError);
-      instance.UI.loadDocument("https://pdf-test-sharinpix.s3.eu-west-3.amazonaws.com/pdf_photo_field.pdf");
+      instance.UI.loadDocument("../../files/calculation_field.pdf");
       this.annotManager = instance.Core.annotationManager;
       this.fieldManager = this.annotManager.getFieldManager();
       this.docViewer = instance.Core.documentViewer;
       instance.UI.setFitMode(instance.UI.FitMode.FitWidth); // workaround: keyboard keeps disappearing until zoomed
-      this.docViewer.addEventListener("annotationsLoaded", async () => {
-        console.log("ANNOTATION LOADED");
-        await this.writeToImageField()
-        await this.docViewer.refreshAll();
-        await this.docViewer.updateView();
-      });
     });
-  }
-
-  async writeToImageField() {
-    await this.instance.Core.PDFNet.initialize();
-    this.document = await this.docViewer.getDocument().getPDFDoc();
-    this.elementWriter = await this.instance.Core.PDFNet.ElementWriter.create();
-    const annotations = await this.annotManager.getAnnotationsList();
-    for (const annotation of annotations) {
-      if (
-        annotation instanceof this.instance.Core.Annotations.WidgetAnnotation
-      ) {
-        if (annotation.getField().name === "sp_photo_6") {
-          this.makeAnnotationTransparent(annotation);
-          console.log("ANNOTATING");
-          const rect = annotation.getRect();
-          const img = await this.createPDFNetImageFromMedia();
-          const pageNum = annotation.getPageNumber();
-          const page = await this.document.getPage(pageNum);
-          const pageHeight = this.docViewer.getPageHeight(pageNum);
-          const builder = await this.instance.Core.PDFNet.ElementBuilder.create();
-          console.log("rect: ", rect);
-          console.log("pageHeight: ", pageHeight);
-          console.log("img: ", img);
-          console.log("rect width: ", rect.getWidth());
-          console.log("rect height: ", rect.getHeight());
-          const element = await builder.createImageScaled(
-            img,
-            rect.x1,
-            pageHeight - rect.y2,
-            rect.getWidth(),
-            rect.getHeight()
-          );
-          console.log("Before beginOnPage");
-          this.elementWriter.beginOnPage(
-            page,
-            this.instance.Core.PDFNet.ElementWriter.WriteMode.e_overlay
-          );
-          console.log("After begin and before writePlacedElement");
-          await this.elementWriter.writePlacedElement(element);
-          console.log("After writePlacedElement");
-          await this.elementWriter.end();
-          console.log("After end");
-          break;
-        }
-      }
-    }
-  }
-
-  async createPDFNetImageFromMedia(
-  ): Promise<Core.PDFNet.Image> {
-    return await this.instance.Core.PDFNet.Image.createFromURL(
-      this.document,
-      "../../files/image.jpg"
-    );
-  }
-
-  makeAnnotationTransparent(
-    annotation: Core.Annotations.WidgetAnnotation
-  ): void {
-    console.log("Making annotation transparent");
-    const transparentColor = new this.instance.Core.Annotations.Color(
-      0,
-      0,
-      0,
-      0
-    );
-    annotation.backgroundColor = transparentColor;
-    const border = new this.instance.Core.Annotations.Border();
-    border.color = transparentColor;
-    annotation.border = border;
-    const font = new this.instance.Core.Annotations.Font();
-    font.fillColor = transparentColor;
-    annotation.font = font;
   }
 }
